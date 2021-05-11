2b9f9311d8d3e7cc87c31530eff79472fc585eae
PetyaKatsarova <pskpetya@yahoo.com>
2021-05-11 17:02:09 +0200

    more code

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+
+// on req by client get: 
 app.get('/', function(req,res){
-    res.send("Hello W!");
+   res.send("Hello W!");
 });
-app.listen(3000);
+// :bla on the url is params: whatever in this func will show
+app.get('/catalog/:producer/:serial_number', (req, res)=>{
+   let products = ['123', '456', '789'];
+   let sn = req.params.serial_number;
+   let producer = req.params.producer;
+   if(products.find(x=>x==sn)){
+      res.send(`<h1>Catalog Page</h1><p>Product S/N: ${sn} with producer ${producer}</p>`);
+   }else{
+      res.status(404);
+      res.send('<p>Error 404: Product not found</p>');
+   }
+   console.log(req.params); // {serail_number: '123'}
+});
+app.listen(port, function(){
+   console.log(`Server listening on port ${port}`);
+});
